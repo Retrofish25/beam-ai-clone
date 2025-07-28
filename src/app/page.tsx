7263d17ddd7b94f572459d4f180f5844c15578a3
Retrofish25 <Retrofishco@gmail.com>
2025-07-28 18:57:37 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
         <WorkflowsSection />
         <BenefitsSection />
         <AgentOSSection />
-        <MultiAgentSection />
         <NewsSection />
         <CTASection />
         <FAQSection />
